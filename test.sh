7fae12b6fecf5a414eb2ba4b9411b0f0de60f48f
DIPANJAN ROY <dipanjanroy369@gmail.com>
2022-07-13 01:41:34 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -11,5 +11,19 @@ then
     echo " " on | mutt -s " " : $var at 'date +%Y_%m_%d_%H:%M:%S' dipanjanroy369@gmail.com"
 
 fi
-
+.....................................
 ....................................................................
+#Initializing two variables
+var=$(ps -af | grep -w pid | awk '{print $2}')
+b=20
+
+echo boom boom $var
+
+if [ $var == $b ]
+then
+    #If they are equal then print this
+    echo "a is fc equal to b"
+else
+    #else print this
+    echo "Weblogic has stopped"
+fi
