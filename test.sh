6f292058c2758dc3c76a25f1e827d3bb92900b94
DIPANJAN ROY <dipanjanroy369@gmail.com>
2022-07-12 12:02:56 +0530

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,15 @@
+var=''
+
+alert=""
+
+if[ $var = "alert" ];
+
+then 
+
+    echo "This is just testing $var"
+
+    echo " " on | mutt -s " " : $var at 'date +%Y_%m_%d_%H:%M:%S' dipanjanroy369@gmail.com"
+
+fi
+
+....................................................................
